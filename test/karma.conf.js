4b384fcbeb46aab7fbd3e587a1ca1d68f5af5b42
Jonathan Hartman <jhartman86@gmail.com>
2017-04-04 02:05:45 -0600

    Squashed commit of the following:
    commit 77eb735890e66578e94c6943ff9fb24875e9e30a
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 01:52:05 2017 -0600
    
        Switched SauceLabs -> Browserstack
    
    commit a81909036fe7c0a5168cfdbe8527e60ec69f448a
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:59:51 2017 -0600
    
        Use TRAVIS_JOB_NUMBER as tunnelIdentifier
    
    commit 65f837bc6e141a312d3cc0c085de88f8202e4ca3
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:45:42 2017 -0600
    
        More sauceLabs connection fun
    
    commit f8274b48454c6e5f2a4580b22776b4600b9ad3d0
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:41:23 2017 -0600
    
        SauceLabs... again :(
    
    commit d47520403d21caf8a7ad052c19c8e18f219d80fb
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:36:33 2017 -0600
    
        SauceLabs connection bugs
    
    commit 51883a26332018b34db46c5b96157b81b8b987c7
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:23:58 2017 -0600
    
        SauceLabs connection issues
    
    commit be5cf56690f8206eb964a1b558117185dff46755
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Tue Apr 4 00:08:19 2017 -0600
    
        object.assign polyfill and IE10/edge tests
    
    commit 7edaab953103648e39292990b35a49fb21883f64
    Author: Jonathan Hartman <jhartman86@gmail.com>
    Date:   Mon Apr 3 19:36:04 2017 -0600
    
        Integrated saucelabs for IE testing; Karma updates

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,12 +7,31 @@
 // https://x-team.com/blog/setting-up-javascript-testing-tools-for-es6/
 // https://medium.com/@scbarrus/how-to-get-test-coverage-on-react-with-karma-babel-and-webpack-c9273d805063#.l96qr7dym
 
+// if BS_USERNAME or BS_ACCESS_KEY env variables aren't set, look locally
+if (!process.env.BS_USERNAME || !process.env.BS_ACCESS_KEY) {
+  const bsCredentials = require('../.browserstack.json');
+  process.env.BS_USERNAME = bsCredentials.username;
+  process.env.BS_ACCESS_KEY = bsCredentials.accessKey;
+}
+
 module.exports = function (config) {
 
-  var _browsers = ['Chrome', 'Firefox'];
+  var _browsers = [
+    'Chrome',
+    'Firefox',
+    'BS_IE_10',
+    'BS_IE_11',
+    'BS_Edge'
+  ];
 
   if (process.env.TRAVIS) {
-    _browsers = ['Chrome_travis_ci', 'Firefox'];
+    _browsers = [
+      'Chrome_travis_ci',
+      'Firefox',
+      'BS_IE_10',
+      'BS_IE_11',
+      'BS_Edge'
+    ];
   }
 
   config.set({
@@ -41,18 +60,15 @@ module.exports = function (config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha'],
 
-
     // list of files / patterns to load in the browser
     files: [
       'tests.webpack.js',
       '../dist/style.css'
     ],
 
-
     // list of files to exclude
     exclude: [
     ],
-
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -60,11 +76,16 @@ module.exports = function (config) {
       'tests.webpack.js': ['webpack'],
     },
 
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha', 'coverage'],
+    reporters: ['mocha', 'coverage', 'BrowserStack'],
+
+    // BrowserStack configuration (for IE testing)
+    browserStack: {
+      name: 'internetips',
+      username: process.env.BS_USERNAME,
+      accessKey: process.env.BS_ACCESS_KEY
+    },
+    captureTimeout: 20000,
 
     coverageReporter: {
       type: 'text-summary'
@@ -73,30 +94,45 @@ module.exports = function (config) {
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
+    logLevel: process.env.TRAVIS ? config.LOG_DEBUG : config.LOG_INFO,
 
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    // browsers: ['Chrome', 'Firefox', 'PhantomJS', 'IE'],
-    // browsers: ['Chrome', 'Firefox'],
     browsers: _browsers,
 
     customLaunchers: {
       Chrome_travis_ci: {
         base: 'Chrome',
         flags: ['--no-sandbox']
+      },
+      BS_IE_10: {
+        base: 'BrowserStack',
+        browser: 'ie',
+        browser_version: '10',
+        os: 'Windows',
+        os_version: '7'
+      },
+      BS_IE_11: {
+        base: 'BrowserStack',
+        browser: 'ie',
+        browser_version: '11',
+        os: 'Windows',
+        os_version: '7'
+      },
+      BS_Edge: {
+        base: 'BrowserStack',
+        browser: 'edge',
+        browser_version: '14',
+        os: 'Windows',
+        os_version: '10'
       }
     },
 
@@ -108,6 +144,7 @@ module.exports = function (config) {
     // how many browser should be started simultaneous
     concurrency: Infinity,
 
+    // webpack configs
     webpack: {
       module: {
         rules: [
@@ -130,19 +167,8 @@ module.exports = function (config) {
         ]
       }
     },
-
     webpackServer: {
       noInfo: true
-    },
-
-    plugins: [
-      require('karma-webpack'),
-      require('istanbul-instrumenter-loader'),
-      require('karma-mocha'),
-      require('karma-mocha-reporter'),
-      require('karma-coverage'),
-      require('karma-chrome-launcher'),
-      require('karma-firefox-launcher')
-    ]
+    }
   });
 };
